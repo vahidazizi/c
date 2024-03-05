2b39c827eb868d4fb8d00c47691f68b35d49f842
vahidazizi <vahid.co2013@gmail.com>
2024-03-05 06:58:17 +0330

    Update air.py

--- air.py
+++ air.py
@@ -1,67 +1,62 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from captcha.image import ImageCaptcha
-import random
-import string
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # ذخیره امتیازات کاربران
 user_scores = {}
 
-# ذخیره کدهای رفرال
-referral_codes = {}
-
-# ساخت تصویر کپچا
-def generate_captcha():
-    captcha_text = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
-    image = ImageCaptcha()
-    data = image.generate(captcha_text)
-    return captcha_text, data
-
 # دستور برای شروع بازی و گرفتن امتیاز
-def start_game(update, context):
-    user_id = update.message.from_user.id
+def start_game(update: Update, context: CallbackContext) -> None:
+    user_id = update.effective_user.id
     if user_id not in user_scores:
         user_scores[user_id] = 0
     update.message.reply_text('بازی شروع شد! امتیاز شما: {}'.format(user_scores[user_id]))
 
 # دستور برای نمایش امتیاز کاربر
-def show_score(update, context):
-    user_id = update.message.from_user.id
+def show_score(update: Update, context: CallbackContext) -> None:
+    user_id = update.effective_user.id
     if user_id in user_scores:
         update.message.reply_text('امتیاز شما: {}'.format(user_scores[user_id]))
     else:
         update.message.reply_text('شما هنوز بازی نکرده‌اید!')
 
 # دستور برای تبدیل امتیاز به کد هدیه
-def redeem_gift(update, context):
+def redeem_gift(update: Update, context: CallbackContext) -> None:
     # کد هدیه را از امتیاز کاربر تولید کنید و به کاربر ارسال کنید
     pass  # به عنوان مثال، اینجا یک عملیات دیگر اضافه کنید
 
 # دستور برای ارتباط با پشتیبانی
-def support(update, context):
+def support(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('برای ارتباط با پشتیبانی، ایمیل بفرستید به: support@example.com')
 
 # دستور برای ایجاد کد رفرال
-def generate_referral_code(update, context):
+def generate_referral_code(update: Update, context: CallbackContext) -> None:
     referral_code = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
     referral_codes[update.message.from_user.id] = referral_code
     update.message.reply_text('کد رفرال شما: {}'.format(referral_code))
 
 # دستور برای نمایش کپچا
-def show_captcha(update, context):
+def show_captcha(update: Update, context: CallbackContext) -> None:
     captcha_text, data = generate_captcha()
     update.message.reply_text('کد کپچا: {}'.format(captcha_text))
     update.message.reply_photo(photo=data)
 
-# افزودن دستورها به ربات
-updater = Updater("6618575314:AAEhdwtRFhFelEvQDUn11n5bpZiwe4OEjUs", use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler("start", start_game))
-dispatcher.add_handler(CommandHandler("score", show_score))
-dispatcher.add_handler(CommandHandler("redeem", redeem_gift))
-dispatcher.add_handler(CommandHandler("support", support))
-dispatcher.add_handler(CommandHandler("referral", generate_referral_code))
-dispatcher.add_handler(CommandHandler("captcha", show_captcha))
+# تابع اصلی برای شروع ربات
+def main() -> None:
+    # تنظیم و شروع ربات
+    updater = Updater("TOKEN")
+    dispatcher = updater.dispatcher
 
-# شروع ربات
-updater.start_polling()
-updater.idle()
+    # افزودن دستورها به ربات
+    dispatcher.add_handler(CommandHandler("start", start_game))
+    dispatcher.add_handler(CommandHandler("score", show_score))
+    dispatcher.add_handler(CommandHandler("redeem", redeem_gift))
+    dispatcher.add_handler(CommandHandler("support", support))
+    dispatcher.add_handler(CommandHandler("referral", generate_referral_code))
+    dispatcher.add_handler(CommandHandler("captcha", show_captcha))
+
+    # شروع ربات
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
